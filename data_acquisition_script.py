22b0b81232b1ca28aed5ef1bb4a628ca84657320
anshsingal <59569924+anshsingal@users.noreply.github.com>
2024-09-06 18:03:10 -0500

    Update data_acquisition_script.py

--- data_acquisition_script.py
+++ data_acquisition_script.py
@@ -5,6 +5,11 @@ print("\n".join(sys.argv))
 import os
 print(os.environ['TOKEN'])
 
+import requests
+response = requests.get(url = "https://api.github.com/repos/anshsingal/sequence-analytics/traffic/clones", headers = {"Accept": "application/vnd.github+json", "Authorization": f"Bearer {os.environ['TOKEN']}", "X-GitHub-Api-Version": "2022-11-28"})
+print("direct access:")
+print(response.json())
 
+print("indirwecrt access")
 f = open("temp_clones.txt", "r")
 print(f.read())
