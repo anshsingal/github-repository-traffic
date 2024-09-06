6e6972193469f0eb17b1d84a1c7225720a6d43b0
anshsingal <59569924+anshsingal@users.noreply.github.com>
2024-09-06 18:19:12 -0500

    Update data_acquisition_script.py

--- data_acquisition_script.py
+++ data_acquisition_script.py
@@ -1,15 +1,18 @@
-import sys
-print("inline arguments are:")
-print("\n".join(sys.argv))
+# import sys
+# print("inline arguments are:")
+# print("\n".join(sys.argv))
 
 import os
-print(os.environ['TOKEN'])
+PAT = os.environ['PERSONAL_ACCESS_TOKEN']
 
 import requests
-response = requests.get(url = "https://api.github.com/repos/anshsingal/sequence-analytics/traffic/clones", headers = {"Accept": "application/vnd.github+json", "Authorization": f"Bearer {os.environ['TOKEN']}", "X-GitHub-Api-Version": "2022-11-28"})
-print("direct access:")
-print(response.json())
+headers = {"Accept": "application/vnd.github+json", "Authorization": f"Bearer {PAT}", "X-GitHub-Api-Version": "2022-11-28"}
+repo_url = "https://api.github.com/repos/anshsingal/sequence-analytics"
 
-print("indirwecrt access")
-f = open("temp_clones.txt", "r")
-print(f.read())
+clones = requests.get(url = repo_url+"/traffic/clones", headers = headers).json()
+print("direct access:")
+print(clones)
+
+# print("indirwecrt access")
+# f = open("temp_clones.txt", "r")
+# print(f.read())
