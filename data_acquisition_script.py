d759c27ddd9f9761831ae0f71c770daf2f46df3d
anshsingal <59569924+anshsingal@users.noreply.github.com>
2024-09-06 16:50:41 -0500

    Update data_acquisition_script.py

--- data_acquisition_script.py
+++ data_acquisition_script.py
@@ -1,3 +1,5 @@
 import sys
-print("inline arguments are:")
-print("\n".join(sys.argv))
+# print("inline arguments are:")
+# print("\n".join(sys.argv))
+f = open("temp_clones.txt", "r")
+print(f.read())
